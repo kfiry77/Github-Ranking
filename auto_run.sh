5fad5105f63cb56001687a76a41d4acf5d3442a0
Kfir Yehezkel <kfir.ye@gmail.com>
2022-11-19 00:12:19 +0200

    merged build scripts with master, removed intermediate files.

--- auto_run.sh
+++ auto_run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # crontab auto run this file
 # crontab command:
-# 0 12 * * * nohup sh /home/kfiry/Github-Ranking/auto_run.sh >> /home/kfirt/tmp/Github-Ranking.log 2>&1 &
+# 0 12 * * * nohup sh /home/kfiry/Github-Ranking/auto_run.sh >> /home/kfiry/tmp/Github-Ranking.log 2>&1 &
 
 echo -e "\n----------Run Time:----------"
 date
 cd /home/kfiry/Github-Ranking
 git pull
-python source/process.py
+python3 source/process.py
 git add .
 today=`date +"%Y-%m-%d"`
 git commit -m "auto update $today"
