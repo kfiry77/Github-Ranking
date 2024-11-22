5248a35c73402367c19d8b96e9446afc97ec6930
Kfir Ezekiel <kfir.ye@gmail.com>
2024-11-22 18:52:16 +0200

    auto update 2024-11-22

--- auto_run.sh
+++ auto_run.sh
@@ -7,9 +7,9 @@ echo -e "\n----------Run Time:----------"
 date
 cd /home/kfiry/Github-Ranking
 git pull
-source .venv/bin/activate
+.venv/bin/activate
 #python3 source/process.py
 git add .
 today=`date +"%Y-%m-%d"`
 git commit -m "auto update $today"
-git push origin master
+#git push origin master
